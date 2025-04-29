cefb02367a23d075c06b891fe400794382fbb98c
shahidluhaif <shahid@luhaifdigitech.com>
2025-04-29 10:23:03 +0530

    action-fix

--- src/test/java/website/ContactTest.java
+++ src/test/java/website/ContactTest.java
@@ -18,7 +18,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class ContactUsTest {
+public class ContactTest {
 
     WebDriver driver;
     WebDriverWait wait;
